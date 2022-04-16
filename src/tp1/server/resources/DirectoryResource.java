ae5a193ecc6e008375b8cdd9af277e0cfdc08088
Joao <ja.jorge@campus.fct.unl.pt>
2022-04-16 14:52:31 +0100

    get file directory

--- src/tp1/server/resources/DirectoryResource.java
+++ src/tp1/server/resources/DirectoryResource.java
@@ -10,6 +10,7 @@ import tp1.clients.RestFileClient;
 import tp1.clients.RestUsersClient;
 import tp1.server.Discovery;
 
+import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.*;
@@ -81,27 +82,55 @@ public class DirectoryResource implements RestDirectory {
         //user server
         URI[] userURI = d.knownUrisOf("users");
         RestUsersClient users = new RestUsersClient(userURI[0]);
-        User user = users.getUser(userId, password);
+        User user = users.getUser(accUserId, password);
 
-        //choose file server
-        URI[] fileURI = d.knownUrisOf("files");
-        Random r = new Random();
-        int result = r.nextInt(fileURI.length);
+        if(!directory.containsKey(userId)) {
+            Log.info("User with userid does not have files.");
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
 
+        FileInfo fI = getFileInfoUser(userId, password);
+        if(fI == null){
+            Log.info("File does not exist.");
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
 
-        directory.get(userId).add(newFile);
+        if( userId != accUserId && !sharedFile(fI,accUserId)){
+            Log.info("User does not have access to file.");
+            throw new WebApplicationException(Response.Status.FORBIDDEN);
+        }
 
-        //add file to file server
-        RestFileClient files = new RestFileClient(fileURI[result]);
+        //get File from file server
+        try {
+            RestFileClient files = new RestFileClient(new URI(fI.getFileURL()));
+            String filedId = String.format("%s/%s", userId, filename);
+            Log.info("URL: "+ fI.getFileURL()+" FILEID: "+filedId);
+            return files.getFile(filedId, "");
 
-        String filedId = String.format("%s/%s", userId, filename);
-        files.writeFile(filedId, data, "");
-
-        return new byte[0];
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
     public List<FileInfo> lsFile(String userId, String password) {
         return null;
+    }
+
+    private FileInfo getFileInfoUser(String userId, String filename){
+        List<FileInfo> files = directory.get(userId);
+
+        for ( FileInfo file : files) {
+            if( file.getFilename().equals(filename))
+                return file;
+        }
+
+        return null;
+    }
+
+    private boolean sharedFile(FileInfo fI, String accUserId){
+        Set<String> sharedWith = fI.getSharedWith();
+        return sharedWith.contains(accUserId);
     }
 }
